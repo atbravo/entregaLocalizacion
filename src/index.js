a3b5a84e04155ae0b477e87fb6fab6ea5c4576bf
atbravo <at.bravo@uniandes.edu.co>
2022-11-22 22:37:23 -0500

    entrega

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import JobsList from "./components/jobsList";
+
+let i18nConfig = {
+    locale: 'en',
+    messages: localeEsMessages
+};
+
+function onChangeLanguage(lang){
+    switch (lang) {
+        case 'es': i18nConfig.messages = localeEsMessages; break;
+        case 'en': i18nConfig.messages = localeEnMessages; break;
+        default: i18nConfig.messages = localeEsMessages; break;
+    }
+    i18nConfig.locale = lang;
+}
+
+onChangeLanguage(navigator.language)
+ReactDOM.render(
+    <IntlProvider locale={i18nConfig.locale} messages= {i18nConfig.messages}>
+    <JobsList/>
+</IntlProvider>, document.getElementById("root")
+);
